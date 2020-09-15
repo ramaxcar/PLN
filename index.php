8e7f53379b12fc2c955d67a36e49bed8c8d08e4b
Zulfan Nurrahman <zulfan2299@gmail.com>
2020-09-15 23:43:11 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+	<title>Home</title>
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
+
+</head>
+<body>
+
+    <div class="container">
+        <form action="#" method="POST">
+            <div class="col-md-10 col-md-offset-5">
+                <h1 class="judul">Monitoring Wilayah Kerja STI JABAR</h1>
+                <hr class="judul">
+                <table class="head">
+                    <tr>
+                    	<th colspan="2">JABAR 1</th>
+                    	<th colspan="2">JABAR 2</th>
+                    </tr>
+                </table>
+                <hr class="tabel" align="center">
+                <table class="body">
+                    <tr>
+                    	<td>Level 1 :</td>
+                    	<td>1. UID JABAR</td>
+                    	<td>Level 1 :</td>
+                    	<td>1. KANTOR INDUK UIT JBT</td>
+                    </tr>
+                    <tr>
+                    	<td></td>
+                    	<td>2. UIP JBT 1</td>
+                    	<td></td>
+                    	<td>2. UPP KIT JBT 1 (CISOKAN)</td>
+                    </tr>
+                    <tr>
+                    	<td></td>
+                    	<td>3. UPP KIT JBT 2 (JATIGEDE)</td>
+                    	<td></td>
+                    	<td>3. UPP KIT JBT 3 (BANJAR)</td>
+                    </tr>
+                    <tr>
+                    	<td></td>
+                    	<td>4. UPP KIT JBT 4 (INDRAMAYU)</td>
+                    	<td></td>
+                    	<td>4. KANTOR INDUK PUSHARLIS</td>
+                    </tr>
+                    <tr>
+                    	<td></td>
+                    	<td></td>
+                    	<td></td>
+                    	<td>5. UP2W3 BANDUNG</td>
+                    </tr>
+                    <tr>
+                    	<td></td>
+                    	<td></td>
+                    	<td></td>
+                    	<td>6. UP2W3 DAYEUHKOLOT</td>
+                    </tr>
+                    <tr>
+                    	<td></td>
+                    	<td></td>
+                    	<td></td>
+                    	<td>7. BIP HUKUM</td>
+                    </tr>
+                    <tr>
+                    	<td></td>
+                    	<td></td>
+                    	<td></td>
+                    	<td>8. SPI REGIONAL 9</td>
+                    </tr>
+                    <tr>
+                    	<td></td>
+                    	<td></td>
+                    	<td></td>
+                    	<td>9. UP2B JABAR</td>
+                    </tr>
+                </table>
+            </div>
+        </form>
+    </div>
+
+</body>
+</html>
