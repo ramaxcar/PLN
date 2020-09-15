6e51cc9640da85352ccc7cf0cb6a544fe6756709
Zulfan Nurrahman <zulfan2299@gmail.com>
2020-09-16 00:11:12 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 
 	<title>Home</title>
 	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
-    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
+    	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
 
 </head>
 <body>
