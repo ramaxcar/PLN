7f75aeac9eb58954b2cd1a5f799e247216d05521
Zulfan Nurrahman <zulfan2299@gmail.com>
2020-09-15 23:56:11 +0700

    Update index.php

--- index.php
+++ index.php
@@ -14,13 +14,16 @@
             <div class="col-md-10 col-md-offset-5">
                 <h1 class="judul">Monitoring Wilayah Kerja STI JABAR</h1>
                 <hr class="judul">
+
                 <table class="head">
                     <tr>
                     	<th colspan="2">JABAR 1</th>
                     	<th colspan="2">JABAR 2</th>
                     </tr>
                 </table>
+
                 <hr class="tabel" align="center">
+
                 <table class="body">
                     <tr>
                     	<td>Level 1 :</td>
@@ -76,7 +79,62 @@
                     	<td></td>
                     	<td>9. UP2B JABAR</td>
                     </tr>
+                    <tr>
+                    	<td>&nbsp</td>
+                    	<td>&nbsp</td>
+                    	<td>&nbsp</td>
+                    	<td>&nbsp</td>
+                    </tr>
+                    <tr>
+                    	<td>Level 2 :</td>
+                    	<td>1. UPT CIREBON</td>
+                    	<td>Level 2 :</td>
+                    	<td>1. UP3 BANDUNG</td>
+                    </tr>
+                    <tr>
+                    	<td></td>
+                    	<td>2. UPT KARAWANG</td>
+                    	<td></td>
+                    	<td>2. UP3 TASIKMALAYA</td>
+                    </tr>
+                    <tr>
+                    	<td></td>
+                    	<td>3. UP3 CIREBON</td>
+                    	<td></td>
+                    	<td>3. UP3 GARUT</td>
+                    </tr>
+                    <tr>
+                    	<td></td>
+                    	<td>4. UP3 INDRAMAYU</td>
+                    	<td></td>
+                    	<td>4. UP3 CIMAHI</td>
+                    </tr>
+                    <tr>
+                    	<td></td>
+                    	<td>5. UP3 SUMEDANG</td>
+                    	<td></td>
+                    	<td>5. UP3 MAJALAYA</td>
+                    </tr>
+                    <tr>
+                    	<td></td>
+                    	<td>6. UP3 PURWAKARTA</td>
+                    	<td></td>
+                    	<td>6. UP3 CIANJUR</td>
+                    </tr>
+                    <tr>
+                    	<td></td>
+                    	<td>7. UP3 KARAWANG</td>
+                    	<td></td>
+                    	<td>7. UP3 SUKABUMI</td>
+                    </tr>
+                    <tr>
+                    	<td></td>
+                    	<td>8. UP2D JABAR</td>
+                    	<td></td>
+                    	<td>8. UPT BANDUNG</td>
+                    </tr>
                 </table>
+
             </div>
         </form>
     </div>
